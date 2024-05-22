0a4930393d6b84aad3360067d36f2f215668a5d0
IfaqYurmama <jbnlp123@gmail.com>
2024-05-22 11:36:04 +0200

    modify symbols

--- symbols.py
+++ symbols.py
@@ -1,31 +1,31 @@
 symbols = [
-	{
-		"name": "MRK",
-		"isin": "DE0006599905",
-		"lots": [
-		{
-			"shares": 13,
-			"cost": 194.11
-		}
-		]
-	},
         {
-                "name": "VWCE",
+                "name": "ETF",
                 "isin": "IE00BK5BQT80",
                 "lots": [
                 {
-                        "shares": 160,
-                        "cost": 94.53
+                        "shares": 740,
+                        "cost": 98.47
                 }
                 ]
         },
-        {
-                "name": "XTP",
-                "isin": "DE0005765507",
+	{
+                "name": "GME",
+                "isin": "US36467W1099",
                 "lots": [
                 {
-                        "shares": 23,
-                        "cost": 90.96
+                        "shares": 0,
+                        "cost": 0
+                }
+                ]
+        },
+	{
+                "name": "IBKR",
+                "isin": "US45841N1072",
+                "lots": [
+                {
+                        "shares": 4,
+                        "cost": 94.01
                 }
                 ]
         },
@@ -39,17 +39,7 @@ symbols = [
                 }
                 ]
         },
-	{
-		"name": "zDAX",
-		"isin": "LU0274211480",
-		"lots": [
-		{
-			"shares": 0,
-			"cost": 0
-		}
-		]
-	},
-	{
+        {
 		"name": ".NASDAQ",
 		"isin": "DE000A0F5UF5",
 		"lots": [
@@ -59,14 +49,4 @@ symbols = [
 		}
 		]
 	},
-        {
-                "name": "zNASDAQ",
-                "isin": "DE000A0F5UF5",
-                "lots": [
-                {
-                        "shares": 0,
-                        "cost": 0
-                }
-                ]
-        },
 ]
